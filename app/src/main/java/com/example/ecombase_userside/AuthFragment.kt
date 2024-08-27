86cf949738eab85422b9e2e838cc3d73e5ab8564
Shivdatt Santosh Jangam <shivdattjangam08@gmail.com>
2024-08-27 07:09:04 +0530

    Auth Done

---
+++ app/src/main/java/com/example/ecombase_userside/AuthFragment.kt
@@ -0,0 +1,61 @@
+package com.example.ecombase_userside
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.ecombase_userside.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAuthBinding.inflate(layoutInflater)
+        getUserNumber()
+        actionOnCont()
+        return binding.root
+    }
+
+    private fun actionOnCont() {
+        binding.btnContinue.setOnClickListener{
+            val number = binding.etUserNumber.text.toString()
+            if(number.isEmpty() || number.length!=10)
+                Utils.showToast(requireContext(),"Please Enter a Valid Number")
+            else{
+                val bundle = Bundle()
+                bundle.putString("number",number)
+                findNavController().navigate(R.id.action_authFragment_to_authOtpFragment,bundle)
+            }
+        }
+    }
+
+    private fun getUserNumber() {
+        binding.etUserNumber.addTextChangedListener( object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val len = s?.length
+                if(len==10)
+                    binding.btnContinue.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.success))
+                else
+                    binding.btnContinue.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.text_muted))
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/ecombase_userside/SplashFragment.kt
@@ -0,0 +1,26 @@
+package com.example.ecombase_userside
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.ecombase_userside.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_authFragment)
+        },4000)
+        return binding.root
+    }
+}
