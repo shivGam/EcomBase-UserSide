6134dd608ed1acb514e29e00ca7d121cee93845f
Shivdatt Santosh Jangam <shivdattjangam08@gmail.com>
2024-08-28 01:41:06 +0530

    Auth OTP Done

--- app/src/main/java/com/example/ecombase_userside/AuthOtpFragment.kt
+++ app/src/main/java/com/example/ecombase_userside/AuthOtpFragment.kt
@@ -1,59 +1,70 @@
 package com.example.ecombase_userside
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.ecombase_userside.databinding.FragmentAuthOtpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AuthOtpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AuthOtpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentAuthOtpBinding
+    private lateinit var userName : String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth_otp, container, false)
+    ): View {
+        binding = FragmentAuthOtpBinding.inflate(layoutInflater)
+        getUserNumber()
+        actionBackButton()
+        requiredIncrement()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AuthOtpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AuthOtpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun actionBackButton() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_authOtpFragment_to_authFragment)
+        }
     }
+
+    private fun requiredIncrement() {
+        val editText = arrayOf(binding.otp1, binding.otp2, binding.otp3, binding.otp4, binding.otp5, binding.otp6)
+
+        for (i in editText.indices) {
+            editText[i].addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    // No implementation needed
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // No implementation needed
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 1) {
+                        if (i < editText.size - 1) {
+                            editText[i + 1].requestFocus()
+                        }
+                    } else if (s?.length == 0) {
+                        if (i > 0) {
+                            editText[i - 1].requestFocus()
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+
+
+
+    private fun getUserNumber() {
+        val bundle = arguments
+        userName = bundle?.getString("number").toString()
+        binding.tvUserPhoneNumber.text = userName
+    }
+
 }
